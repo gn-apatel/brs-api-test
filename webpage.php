3baef4f871287624b8a092586f6f2fdd492b239f
gn-apatel <anika.patel@nbcuni.com>
2023-07-25 16:38:43 +0100

    added comments and camelcase variable names

--- webpage.php
+++ webpage.php
@@ -15,8 +15,8 @@ $response = $client->get('https://www.brsgolf.com/api/v2/clubs');
 
 if (200 == $response->getStatusCode()) {            // 200 status code = success
     $output = $response->getBody();
-    $response_data = json_decode($output);          // decodes data from json and converts it to objects
-    $club_data = $response_data->_results;  }        // removes header/extra info contained within the class
+    $responseData = json_decode($output);          // decodes data from json and converts it to objects
+    $clubData = $responseData->_results;  }        // removes header/extra info contained within the class
 
 ?>
 
@@ -26,6 +26,7 @@ if (200 == $response->getStatusCode()) {            // 200 status code = success
 
 <head>
     <meta charset="UTF-8">
+    <!-- line below brings in bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <title>Mobile-Enabled Clubs</title>
 
@@ -49,8 +50,9 @@ if (200 == $response->getStatusCode()) {            // 200 status code = success
     <div class="container">
 
         <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+        <!-- Outputs as columns of 3 -->
 
-<?php foreach ($club_data as $club):
+<?php foreach ($clubData as $club):             // loops through all clubs
         if ($club->mobile_enabled): ?>
 
                         <div class="col">
@@ -61,21 +63,25 @@ if (200 == $response->getStatusCode()) {            // 200 status code = success
                                 <div class="card-body">
                                     <p class="card-text">
                                         <?php
-                                        foreach($club->address as $addressline){
-                                            if ($addressline != "") {
-                                                echo $addressline .", ";
+                                        // outputs each property contained in address object
+                                        foreach($club->address as $addressLine){
+                                            if ($addressLine != "") {
+                                                echo $addressLine .", ";
+                                                // if address line value is empty, does not bother outputting it out
                                             }
                                         }
                                         ?>
                                     </p>
                                     <div class="d-flex justify-content-between align-items-center">
                                         <div class="btn-group">
-                                            <?php $club_url = "https://www.brsgolf.com/" . $club->club_id; ?>
-                                            <a href="<?php echo $club_url ?>">
+                                            <?php $clubURL = "https://www.brsgolf.com/" . $club->club_id; ?>
+                                            <!-- Attaches hyperlink to button -->
+                                            <a href="<?php echo $clubURL ?>">
                                                 <button type="button" class="btn btn-sm btn-outline-secondary">Visit</button>
                                             </a>
                                         </div>
                                         <small class="text-body-secondary">(<?php echo $club->club_id ?>)</small>
+                                        <!-- retrieves club ID -->
                                     </div>
                                 </div>
                             </div>
